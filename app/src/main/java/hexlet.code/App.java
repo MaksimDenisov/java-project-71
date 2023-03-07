5e6710db30289c067ab39f76b572a2e123a0d5a1
max <max.nick.demax@gmail.com>
2023-03-07 06:16:54 +0300

    complete 4

--- app/src/main/java/hexlet.code/App.java
+++ app/src/main/java/hexlet.code/App.java
@@ -2,10 +2,20 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true,
         description = "Compares two configuration files and shows a difference.")
 public class App implements Runnable {
+    @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
+    private String filepath1;
+    @Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
+    private String filepath2;
+
+
+    @Option(names = {"-f", "--format"}, paramLabel = "format", description = "output format [default: stylish]")
+    private String format = "stylish";
 
     public static void main(String[] args) {
         new CommandLine(new App()).execute(args);
