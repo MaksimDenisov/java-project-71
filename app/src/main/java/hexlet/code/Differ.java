c5b82d57d0f0c7d446401f2732e7d65601ebb695
max <max.nick.demax@gmail.com>
2023-03-13 19:03:07 +0300

    extract generateDiffs

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -3,19 +3,19 @@ package hexlet.code;
 
 import hexlet.code.formatters.Formatter;
 
-import java.util.Map;
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
-import java.util.ArrayList;
+import java.util.*;
 
 public class Differ {
 
-    public static String generate(String filePath1, String filePath2, String format) throws Exception {
-        return Formatter.getFormatter(format).format(generate(filePath1, filePath2));
+    public static String generate(String filePath1, String filePath2) throws Exception {
+        return generate(filePath1, filePath2, Formatter.STYLISH);
     }
 
-    public static List<Diff> generate(String filePath1, String filePath2) throws Exception {
+    public static String generate(String filePath1, String filePath2, String format) throws Exception {
+        return Formatter.getFormatter(format).format(generateDiffs(filePath1, filePath2));
+    }
+
+    private static List<Diff> generateDiffs(String filePath1, String filePath2) throws Exception {
         Map<String, Object> json1 = Parser.parse(filePath1);
         Map<String, Object> json2 = Parser.parse(filePath2);
         Set<String> keys = new TreeSet<>();
