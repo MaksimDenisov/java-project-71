28fb4610057975beee129aa2d2a98b7e13bb2c63
max <max.nick.demax@gmail.com>
2023-03-13 18:29:36 +0300

    complete task 10

---
+++ app/src/main/java/hexlet/code/formatters/JsonFormatter.java
@@ -0,0 +1,22 @@
+package hexlet.code.formatters;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hexlet.code.Diff;
+
+import java.util.List;
+
+public class JsonFormatter  implements Formatter.TextFormatter {
+
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    @Override
+    public String format(List<Diff> diffs) {
+        try {
+            return   mapper.writeValueAsString(diffs);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+}
--- app/src/main/java/hexlet/code/formatters/PlainFormatter.java
+++ app/src/main/java/hexlet/code/formatters/PlainFormatter.java
@@ -6,22 +6,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/*private static final String EXPECTED_PLAIN = """
-            Property 'chars2' was updated. From [complex value] to false
-            Property 'checked' was updated. From false to true
-            Property 'default' was updated. From null to [complex value]
-            Property 'id' was updated. From 45 to null
-            Property 'key1' was removed
-            Property 'key2' was added with value: 'value2'
-            Property 'numbers2' was updated. From [complex value] to [complex value]
-            Property 'numbers3' was removed
-            Property 'numbers4' was added with value: [complex value]
-            Property 'obj1' was added with value: [complex value]
-            Property 'setting1' was updated. From 'Some value' to 'Another value'
-            Property 'setting2' was updated. From 200 to 300
-            Property 'setting3' was updated. From true to 'none'
-            """;*/
-
 public class PlainFormatter implements Formatter.TextFormatter {
     public static final String INDENT = "  ";
     public static final String FORMAT_STRING = "%s: %s\n";
@@ -33,9 +17,9 @@ public class PlainFormatter implements Formatter.TextFormatter {
     private static final Map<Diff.TYPE, String> FORMATS = new HashMap<>();
 
     static {
-        FORMATS.put(Diff.TYPE.IDENTITY, NOT_CHANGE_STRING);
-        FORMATS.put(Diff.TYPE.ADD, ADD_STRING);
-        FORMATS.put(Diff.TYPE.REMOVE, REMOVE_STRING);
+        FORMATS.put(Diff.TYPE.UNCHANGED, NOT_CHANGE_STRING);
+        FORMATS.put(Diff.TYPE.ADDED, ADD_STRING);
+        FORMATS.put(Diff.TYPE.REMOVED, REMOVE_STRING);
         FORMATS.put(Diff.TYPE.UPDATED, UPDATE_STRING);
     }
 
@@ -46,13 +30,13 @@ public class PlainFormatter implements Formatter.TextFormatter {
     }
 
     private String formatLine(Diff diff) {
-        if (diff.getType() == Diff.TYPE.IDENTITY) {
+        if (diff.getType() == Diff.TYPE.UNCHANGED) {
             return "";
         }
         if (diff.getType() == Diff.TYPE.UPDATED) {
-            Diff.Pair pair = (Diff.Pair) diff.getValue();
+            Diff.Changes pair = (Diff.Changes) diff.getValue();
             return String.format(FORMATS.get(diff.getType()), diff.getKey(),
-                    getStringValue(pair.getValue1()), getStringValue(pair.getValue2()));
+                    getStringValue(pair.getOldValue()), getStringValue(pair.getNewValue()));
         } else {
             return String.format(FORMATS.get(diff.getType()), diff.getKey(), getStringValue(diff.getValue()));
         }
